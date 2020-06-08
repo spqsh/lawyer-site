07c14aaccd5ab6efee82676cec43d88d2472e313
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2020-06-08 10:26:11 +0300

    setup for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CssnanoPlugin = require("cssnano-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
